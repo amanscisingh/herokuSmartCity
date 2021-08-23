65ef9bd1291aaab8dca6b803d8736fb91eca8101
Aman Singh <aman.singh.civ19@itbhu.ac.in>
2021-08-23 15:17:08 +0530

    c6

--- config/passport.js
+++ config/passport.js
@@ -8,8 +8,7 @@ function pass (passport) {
     passport.use(new GoogleStrategy({
         clientID : process.env.GOOGLE_CLIENT_ID,
         clientSecret : process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL : '/auth/google/callback',
-        proxy: true
+        callbackURL : 'https://waste-management-007.herokuapp.com/auth/google/callback',
     },
     async (accessToken, refreshToken, profile, done) => {
         const newUser = {
