5f8cc4cf614a05008aa289b36e4c1020092fc5a1
Aman Singh <aman.singh.civ19@itbhu.ac.in>
2021-08-23 15:10:10 +0530

    c5

--- config/passport.js
+++ config/passport.js
@@ -9,6 +9,7 @@ function pass (passport) {
         clientID : process.env.GOOGLE_CLIENT_ID,
         clientSecret : process.env.GOOGLE_CLIENT_SECRET,
         callbackURL : '/auth/google/callback',
+        proxy: true
     },
     async (accessToken, refreshToken, profile, done) => {
         const newUser = {
