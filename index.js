1c824a6d082ae01513f9844b1c3ca2a80c3a1e0a
Aman Singh <aman.singh.civ19@itbhu.ac.in>
2021-08-23 15:28:18 +0530

    c7

--- index.js
+++ index.js
@@ -89,6 +89,29 @@ app.use(methodOverride(function (req, res) {
     }
   }))
 
+
+
+
+
+
+
+
+// ----------------------------
+app.use(function(req, res, next) {
+  if (process.env.NODE_ENV === "production") {
+      const reqType = req.headers["x-forwarded-proto"];
+      // if not https redirect to https unless logging in using OAuth
+      if (reqType !== "https") {
+          req.url.indexOf("auth/google") !== -1
+            ? next()
+            : res.redirect("https://" + req.headers.host + req.url);
+      } 
+  } else {
+      next();
+  }
+});  
+
+
 app.use( express.json() );
 app.use('/', defaultRoute);
 app.use('/get', route);
